63d720a54d29e865f5bf54d9dee87afe99f75626
diegoalvrezz <126705185+diegoalvrezz@users.noreply.github.com>
2025-03-10 21:49:37 +0000

    v90

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,8 @@ authenticator = stauth.Authenticate(
     cookie_expiry_days=1
 )
 
-name, authentication_status, username = authenticator.login("Inicia sesión", location="main")
+name, authentication_status, username = authenticator.login(form_name="Inicia sesión", location="main")
+
 
 
 if authentication_status:
